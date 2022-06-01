169e3aa67656ab784a0d02f90b220f9e8084821f
colby <colbykrawczyk10@gmail.com>
2022-06-01 04:59:12 -0400

    Removed the unusable team colors

--- src/main/java/com/github/firewolf8385/woolwars/game/teams/TeamColor.java
+++ src/main/java/com/github/firewolf8385/woolwars/game/teams/TeamColor.java
@@ -1,19 +1,17 @@
 package com.github.firewolf8385.woolwars.game.teams;
 
 public enum TeamColor {
-    AQUA(),
+    AQUA,
     BLACK,
     BLUE,
     DARK_AQUA,
     DARK_BLUE,
     DARK_GRAY,
-    DARK_GREEN,
-    DARK_RED,
-    GOLD,
-    GRAY,
     GREEN,
+    ORANGE,
+    GRAY,
+    LIME,
     LIGHT_PURPLE,
     RED,
-    WHITE,
     YELLOW;
 }
