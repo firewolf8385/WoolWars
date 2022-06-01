d8d34c1921595a255d528f167f4aab2af42c66e6
colby <colbykrawczyk10@gmail.com>
2022-05-31 20:34:51 -0400

    Adds scoreboard utilities

---
+++ src/main/java/com/github/firewolf8385/woolwars/utilities/scoreboard/CustomScoreboard.java
@@ -0,0 +1,29 @@
+package com.github.firewolf8385.woolwars.utilities.scoreboard;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Represents a scoreborad that is easy
+ * to create and customize.
+ */
+public abstract class CustomScoreboard {
+    private static final HashMap<UUID, CustomScoreboard> players = new HashMap<>();
+
+    public CustomScoreboard(Player p) {
+        ScoreHelper.removeScore(p);
+        players.put(p.getUniqueId(), this);
+    }
+
+    public static HashMap<UUID, CustomScoreboard> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player p) {
+        getPlayers().put(p.getUniqueId(), this);
+    }
+
+    public abstract void update(Player p);
+}
---
+++ src/main/java/com/github/firewolf8385/woolwars/utilities/scoreboard/ScoreboardUpdate.java
@@ -0,0 +1,27 @@
+package com.github.firewolf8385.woolwars.utilities.scoreboard;
+
+import com.github.firewolf8385.woolwars.WoolWars;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * A repeating task to update all active
+ * Custom Scoreboards.
+ */
+public class ScoreboardUpdate extends BukkitRunnable {
+    private final WoolWars plugin;
+
+    public ScoreboardUpdate(WoolWars plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            if(ScoreHelper.hasScore(player)) {
+                CustomScoreboard.getPlayers().get(player.getUniqueId()).update(player);
+            }
+        }
+    }
+}
