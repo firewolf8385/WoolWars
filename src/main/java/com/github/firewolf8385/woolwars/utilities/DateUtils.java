0d4a3bb70bf495cf9fa79ab4c2ee1cfe03a5c498
colby <colbykrawczyk10@gmail.com>
2022-05-31 20:25:07 -0400

    Adds a method to format the current date

---
+++ src/main/java/com/github/firewolf8385/woolwars/utilities/DateUtils.java
@@ -0,0 +1,32 @@
+package com.github.firewolf8385.woolwars.utilities;
+
+import java.time.LocalDate;
+
+public class DateUtils {
+
+    public static String currentDateToString() {
+        String dateString = "";
+
+        // Gets the current date.
+        LocalDate currentdate = LocalDate.now();
+
+        // Adds the month to the formatted string.
+        if(currentdate.getMonthValue() < 10) {
+            dateString += " ";
+        }
+        dateString += currentdate.getMonthValue();
+        dateString += "/";
+
+        // Adds the day to the formatted string.
+        dateString += currentdate.getDayOfMonth();
+        dateString += "/";
+
+        // Adds the year to the formatted string.
+        String year = currentdate.getYear() + "";
+        dateString += year.substring(Math.max(year.length() - 2, 0));
+
+        // Returns the new string.
+        return dateString;
+    }
+
+}
