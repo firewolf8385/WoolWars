cc6a4f572283cfae9dc739b60b28b8cd89a78e5e
colby <colbykrawczyk10@gmail.com>
2022-05-31 22:42:51 -0400

    Added getter methods for WoolPlayer

--- src/main/java/com/github/firewolf8385/woolwars/players/WoolPlayer.java
+++ src/main/java/com/github/firewolf8385/woolwars/players/WoolPlayer.java
@@ -9,12 +9,24 @@ public class WoolPlayer {
     private final UUID playerUUID;
 
     // Data
-    private int coins;
-    private int level;
-    private int experience;
+    private int coins = 0;
+    private int level = 1;
+    private int experience = 0;
 
     public WoolPlayer(WoolWars plugin, UUID playerUUID) {
         this.plugin = plugin;
         this.playerUUID = playerUUID;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getLevel() {
+        return level;
     }
 }
