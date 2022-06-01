e1d4456165d6063d4908438f2ea00f1d69746fd2
colby <colbykrawczyk10@gmail.com>
2022-06-01 03:23:40 -0400

    Adds a list of possible team colors

--- src/main/java/com/github/firewolf8385/woolwars/game/teams/TeamColor.java
+++ src/main/java/com/github/firewolf8385/woolwars/game/teams/TeamColor.java
@@ -1,4 +1,19 @@
 package com.github.firewolf8385.woolwars.game.teams;
 
-public class TeamColor {
+public enum TeamColor {
+    AQUA(),
+    BLACK,
+    BLUE,
+    DARK_AQUA,
+    DARK_BLUE,
+    DARK_GRAY,
+    DARK_GREEN,
+    DARK_RED,
+    GOLD,
+    GRAY,
+    GREEN,
+    LIGHT_PURPLE,
+    RED,
+    WHITE,
+    YELLOW;
 }
