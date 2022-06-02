65e9677aa7fab7a417fe4bc91e8d31f299cd138a
colby <colbykrawczyk10@gmail.com>
2022-06-02 08:39:59 -0400

    Adds a wool color to each TeamColor

--- src/main/java/com/github/firewolf8385/woolwars/game/teams/TeamColor.java
+++ src/main/java/com/github/firewolf8385/woolwars/game/teams/TeamColor.java
@@ -1,5 +1,6 @@
 package com.github.firewolf8385.woolwars.game.teams;
 
+import com.github.firewolf8385.woolwars.utilities.items.XMaterial;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 
@@ -7,24 +8,26 @@ import org.bukkit.Color;
  * Represents a color option for a team.
  */
 public enum TeamColor {
-    RED(Color.RED, ChatColor.RED, "RED"),
-    ORANGE(Color.ORANGE, ChatColor.GOLD, "ORANGE"),
-    YELLOW(Color.YELLOW, ChatColor.YELLOW, "YELLOW"),
-    GREEN(Color.LIME, ChatColor.GREEN, "GREEN"),
-    BLUE(Color.BLUE, ChatColor.BLUE, "BLUE"),
-    AQUA(Color.AQUA, ChatColor.AQUA, "AQUA"),
-    PURPLE(Color.PURPLE, ChatColor.DARK_PURPLE, "PURPLE"),
-    PINK(Color.FUCHSIA, ChatColor.LIGHT_PURPLE, "PINK"),
-    BLACK(Color.BLACK, ChatColor.BLACK, "BLACK");
+    RED(Color.RED, ChatColor.RED, "RED", XMaterial.RED_WOOL),
+    ORANGE(Color.ORANGE, ChatColor.GOLD, "ORANGE", XMaterial.ORANGE_WOOL),
+    YELLOW(Color.YELLOW, ChatColor.YELLOW, "YELLOW", XMaterial.YELLOW_WOOL),
+    GREEN(Color.LIME, ChatColor.GREEN, "GREEN", XMaterial.GREEN_WOOL),
+    BLUE(Color.BLUE, ChatColor.BLUE, "BLUE", XMaterial.BLUE_WOOL),
+    AQUA(Color.AQUA, ChatColor.AQUA, "AQUA", XMaterial.LIGHT_BLUE_WOOL),
+    PURPLE(Color.PURPLE, ChatColor.DARK_PURPLE, "PURPLE", XMaterial.PURPLE_WOOL),
+    PINK(Color.FUCHSIA, ChatColor.LIGHT_PURPLE, "PINK", XMaterial.PINK_WOOL),
+    BLACK(Color.BLACK, ChatColor.BLACK, "BLACK", XMaterial.BLACK_WOOL);
 
     private final Color leatherColor;
     private final ChatColor chatColor;
     private final String name;
+    private XMaterial wool;
 
-    TeamColor(Color leatherColor, ChatColor chatColor, String name) {
+    TeamColor(Color leatherColor, ChatColor chatColor, String name, XMaterial wool) {
         this.leatherColor = leatherColor;
         this.chatColor = chatColor;
         this.name = name;
+        this.wool = wool;
     }
 
     /**
@@ -49,5 +52,13 @@ public enum TeamColor {
      */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Gets the wool color of the team,
+     * @return Wool color.
+     */
+    public XMaterial getWool() {
+        return wool;
     }
 }
